04d5b919452f60fded05ea8ade37fb699588fec1
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-04 12:52:16 -0600

    altered HTML, trying different javascript approach

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-let question = {
-    title: 'questionPlace',
-    alternatives: ['answer1', 'answer2', 'answer3'],
-    correctAnswer: 1
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startQuiz)
+   
+
+function startQuiz() {
+    console.log('Started')
+    startButton.classList.add('hide')
 
 }
 
-const QUESTIONS = [
-    "What is 2+8?", 
-    "How many legs does the spider have?", 
-    "what is the capital of Russia?"
- ];
- 
- let counter = 0;
- let display = document.querySelectora('h3'); // is only targeting 1st <h3> (maybe All?)
- display.innerText = QUESTIONS[counter]
- 
- let btn = document.getElementById('start');
- 
- btn.addEventListener('click', function() {
-    counter++;
-    display.innerText = QUESTIONS[counter]
- })
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
