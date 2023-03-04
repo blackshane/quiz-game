4fb2d7714791725e0d1b053adc7a5df0e390c5e3
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-04 14:52:06 -0600

    a lot of javascript. needs styling work

--- script.js
+++ script.js
@@ -1,18 +1,124 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('question')
+const answerButtonsEl = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
    
 
 function startQuiz() {
-    console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove('hide')
+    nextQuestion()
 
 }
 
 function nextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsEl.firstChild)
+        answerButtonsEl.removeChild
+        (answerButtonsEl.firstChild)
+}
+
+function selectAnswer(event) {
+    const selectedButton = event.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    }
+
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 
 }
 
-function selectAnswer() {
+const questions = [
+{
+    question: 'Question 1 about Javascript',
+    answers: [
+        { text: 'right answer', correct: true },
+        { text: 'wrong answer 1', correct: false },
+        { text: 'wrong answer 2', correct: false },
+        { text: 'wrong answer 3', correct: false },
+    ]
+},
 
+
+{
+    question: 'Question 2 about Javascript',
+    answers: [
+        { text: 'right answer', correct: true },
+        { text: 'wrong answer 1', correct: false },
+        { text: 'wrong answer 2', correct: false },
+        { text: 'wrong answer 3', correct: false },
+    ]
+},
+
+{
+    question: 'Question 3 about Javascript',
+    answers: [
+        { text: 'right answer', correct: true },
+        { text: 'wrong answer 1', correct: false },
+        { text: 'wrong answer 2', correct: false },
+        { text: 'wrong answer 3', correct: false },
+    ]
+},
+{
+    question: 'Question 4 about Javascript',
+    answers: [
+        { text: 'right answer', correct: true },
+        { text: 'wrong answer 1', correct: false },
+        { text: 'wrong answer 2', correct: false },
+        { text: 'wrong answer 3', correct: false },
+    ]
 }
+
+]
